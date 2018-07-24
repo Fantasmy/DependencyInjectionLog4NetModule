62713775d91920043595e81a2420cc3b56dfa093
Jenny <fantazmit@gmail.com>
2018-07-24 21:18:26 +0200

    Finished, can improve with separated log4net config file

---
+++ InyeccionLog4Net/Calculation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using log4net;
+
+namespace InyeccionLog4Net
+{
+    public class Calculation : ICalculation
+    {
+        private readonly log4net.ILog _logger;
+
+        public Calculation(ILog logger)
+        {
+            _logger = logger;
+        }
+
+
+        public int Division(int num1, int num2)
+        {
+            int res = 0;
+
+            try
+            {
+                res = num1 / num2;
+                
+            }
+            catch (Exception)
+            {
+                _logger.Error("Can't divide by 0. -> num1= "+ num1 + ", num = "+ num2);
+                //throw new Exception("No se puede dividir entre 0.");
+            }
+
+            return res;
+        }
+
+    }
+}
